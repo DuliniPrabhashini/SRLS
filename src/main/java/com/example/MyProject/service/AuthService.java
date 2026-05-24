197adaadf0a271b728c4a6d2af2b3b2938403767
DuliniPrabhashini <duliniprabhashini57@gmail.com>
2026-05-24 21:20:46 +0530

    change AuthService.

--- src/main/java/com/example/MyProject/service/AuthService.java
+++ src/main/java/com/example/MyProject/service/AuthService.java
@@ -1,6 +1,8 @@
 package com.example.MyProject.service;
 
+import com.example.MyProject.dto.LoginDTO;
 import com.example.MyProject.dto.RegisterDTO;
+import com.example.MyProject.dto.ResponseDTO;
 import com.example.MyProject.entity.User;
 import com.example.MyProject.repository.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,12 +15,15 @@ public class AuthService {
     private UserRepo userRepo;
 
     // Register
-    public String RegisterUser(User user) {
-        User existingUser = userRepo.findByEmail(user.getEmail());
-
-        if (existingUser != null) {
+    public String RegisterUser(RegisterDTO registerDTO) {
+        if(userRepo.findByEmail(registerDTO.getEmail()) != null) {
             return "Email Already Exists";
         }
+
+        User user = new User();
+        user.setUser_name(registerDTO.getUser_name());
+        user.setPassword(registerDTO.getPassword());
+        user.setEmail(registerDTO.getEmail());
 
         userRepo.save(user);
 
@@ -26,17 +31,32 @@ public class AuthService {
     }
 
     // login
-    public String LoginUser(String email, String password) {
-        User user = userRepo.findByEmail(email);
+    public String LoginUser(LoginDTO loginDTO) {
+        User user = userRepo.findByEmail(loginDTO.getEmail());
 
         if (user == null) {
             return "User not found !";
         }
 
-        if (!user.getPassword().equals(password)) {
+        if (!user.getPassword().equals(loginDTO.getPassword())) {
             return "Wrong password !";
         }
 
         return "User Logged In Successfully";
+    }
+
+    // Get User
+    public ResponseDTO getUser(String email) {
+        User user = userRepo.findByEmail(email);
+
+        if (user == null) {
+            return null;
+        }
+
+        return new ResponseDTO(
+                user.getUser_id(),
+                user.getUser_name(),
+                user.getEmail()
+        );
     }
 }
