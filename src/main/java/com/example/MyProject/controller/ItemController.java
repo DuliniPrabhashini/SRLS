edb1ac1874d25eb7a7e0acbd2b50c0545991160d
DuliniPrabhashini <duliniprabhashini57@gmail.com>
2026-06-18 23:45:15 +0530

    Updated ItemController

--- src/main/java/com/example/MyProject/controller/ItemController.java
+++ src/main/java/com/example/MyProject/controller/ItemController.java
@@ -4,19 +4,17 @@ import com.example.MyProject.dto.ItemDTO;
 import com.example.MyProject.service.ItemService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/item")
-@CrossOrigin
+@CrossOrigin(origins = "*")
 public class ItemController {
 
     @Autowired
     private ItemService itemService;
 
+    @PostMapping("/save")
     public String saveItem(@Valid @RequestBody ItemDTO itemDTO) {
         return itemService.saveItem(itemDTO);
     }
